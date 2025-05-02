4323da719a7fed0900ff738c2f37d76cd6c14641
Iryna Zahorodnia <irishas25@ukr.net>
2025-05-02 17:21:54 +0300

    accounting server

---
+++ src/expenses/expenses.service.js
@@ -0,0 +1,74 @@
+let expenses = [];
+let uniqueId = 1;
+
+function clearExpenses() {
+  expenses = [];
+}
+
+function getAll({ userId: queryUserId, categories, to, from }) {
+  let filteredExpenses = expenses;
+
+  if (queryUserId) {
+    filteredExpenses = filteredExpenses.filter(
+      ({ userId }) => +queryUserId === userId,
+    );
+  }
+
+  if (categories) {
+    const handleFilterByCategories = ({ category }) =>
+      categories.includes(category);
+
+    filteredExpenses = filteredExpenses.filter(handleFilterByCategories);
+  }
+
+  if (from) {
+    filteredExpenses = filteredExpenses.filter(({ spentAt }) => spentAt > from);
+  }
+
+  if (to) {
+    filteredExpenses = filteredExpenses.filter(({ spentAt }) => spentAt < to);
+  }
+
+  return filteredExpenses;
+}
+
+function getById(id) {
+  return expenses.find((expense) => expense.id === id) || null;
+}
+
+function deleteById(id) {
+  expenses = expenses.filter((expense) => expense.id !== id);
+}
+
+function create({ userId, spentAt, title, amount, category, note }) {
+  const newExpense = {
+    id: uniqueId,
+    userId,
+    spentAt,
+    title,
+    amount,
+    category,
+    note,
+  };
+
+  uniqueId += 1;
+
+  expenses.push(newExpense);
+
+  return newExpense;
+}
+
+function update(id, newValues) {
+  const expenseToUpdate = getById(id);
+
+  return Object.assign(expenseToUpdate, { ...newValues });
+}
+
+module.exports = {
+  clearExpenses,
+  getAll,
+  getById,
+  deleteById,
+  create,
+  update,
+};
